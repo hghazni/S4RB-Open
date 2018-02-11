cea1a4a33a282169b098e19815378a635e334106
admin <admin@admins-MacBook-Pro.local>
2018-02-11 17:03:32 +0000

    fetches the db with a get method on HttpClient

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Config } from '../Config';
 
 @Injectable()
 export class ConfigService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+  configUrl = Config.dbURL;
 
+  getConfig() {
+    return this.http.get(this.configUrl);
+  }
 }
